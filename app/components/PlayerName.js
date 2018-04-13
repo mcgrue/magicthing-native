35a58b960ef9e36c47bd54601f9da15828ca4dfc
Ben McGraw <ben.mcgraw@gmail.com>
2018-04-13 08:56:48 -0700

    Framework for name editing

--- app/components/PlayerName.js
+++ app/components/PlayerName.js
@@ -4,12 +4,28 @@ import PropTypes from "prop-types";
 import { Text } from "react-native";
 
 class PlayerName extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.editMode = false;
+  }
+
   render() {
-    return (
+    const display = (
       <Text style={{ fontSize: 28, textAlign: "center" }}>
         {this.props.name}
       </Text>
     );
+
+    const edit = (
+      <Text style={{ fontSize: 28, textAlign: "center" }}>EDIT</Text>
+    );
+
+    if (this.editMode) {
+      return edit;
+    } else {
+      return display;
+    }
   }
 }
 
